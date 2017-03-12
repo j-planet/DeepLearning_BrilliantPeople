5c4574991d348399d3a4e03d979afa67b8744a80
j-planet <jin.yuejenny@gmail.com>
2017-03-11 19:10:03 -0800

    crawled extracts from wikipedia

---
+++ wiki.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from pprint import pprint
+
+def process_one_name(name):
+    name = name.title()
+    url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&meta=&titles=' + name.replace(' ', '+')
+    t = requests.get(url)
+    fullD = json.loads(str(t.content, 'utf-8'))
+    extract = list(fullD['query']['pages'].items())[0][1]['extract']
+
+    # pprint(extract, width=200)
+
+    with open('./data/extracts/' + name + '.txt', 'w', encoding='utf-8') as ofile:
+        ofile.writelines(extract)
+
+# process_one_name('albert einstein')
+
+total = 0
+processed = 0
+failed = 0
+
+for name in json.load(open('./data/processed_names.json')).keys():
+    total += 1
+    try:
+        process_one_name(name)
+    except Exception as e:
+        print('ERROR for name :', name, e)
+        failed += 1
+        continue
+
+    processed += 1
+
+print(processed, ' out of ', total, ' processed successfully.')
