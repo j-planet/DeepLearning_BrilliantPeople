150fa10e7638685a9101cd82fdfa3b55ad87b00f
j-planet <jin.yuejenny@gmail.com>
2017-04-12 18:56:47 -0700

    gitignore: ignoring extracts and early life files from now on. added name jsons

--- data_processing/clean_data.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i backup 's/^people//g' ./data/names.txt
-sed -i backup 's/^king//g' ./data/names.txt
-sed -i backup 's/^historical//g' ./data/names.txt
-sed -i backup 's/historical (/ (/g' ./data/names.txt
-sed -i backup 's/(historical/(/g' ./data/names.txt
