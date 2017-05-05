fcc844c26f978bcc16bcf3c2e4c3523da7426ee7
j-planet <jin.yuejenny@gmail.com>
2017-05-04 22:28:19 -0700

    minor

--- models/mark6.py
+++ models/mark6.py
@@ -9,18 +9,28 @@ from layers.dropout_layer import DropoutLayer
 from utilities import make_params_dict
 
 
+class RNNConfig(object):
+    def __init__(self, numCellUnits, keepProbs, activation=None):
+
+        assert len(numCellUnits)==len(keepProbs)
+        assert min(keepProbs)>0 and max(keepProbs)<=1
+
+        self.numCellUnits = numCellUnits
+        self.keepProbs = keepProbs
+        self.activation = activation
+
 
 class Mark6(AbstractModel):
 
     def __init__(self, input_,
                  initialLearningRate, l2RegLambda,
-                 rnnCellUnitsNProbs,
-                 pooledKeepProb,
+                 rnnConfigs,
+                 pooledKeepProb, pooledActivation,
                  loggerFactory_=None):
         """        
         :type initialLearningRate: float 
         :type l2RegLambda: float
-        :type rnnCellUnitsNProbs: list
+        :type rnnConfigs: list
         :type pooledKeepProb: float 
         """
 
@@ -28,16 +38,17 @@ class Mark6(AbstractModel):
 
         self.l2RegLambda = l2RegLambda
         self.pooledKeepProb = pooledKeepProb
-        self.rnnCellUnitsNProbs = rnnCellUnitsNProbs
+        self.pooledActivation = pooledActivation
+        self.rnnConfigs = rnnConfigs
 
         super().__init__(input_, initialLearningRate, loggerFactory_)
         self.print('l2 reg lambda: %0.7f' % l2RegLambda)
 
     def make_graph(self):
-        makers = [RNNLayer.new(cellUnits, keepProbs, activation='sigmoid') for cellUnits, keepProbs in self.rnnCellUnitsNProbs]
+        makers = [RNNLayer.new(c.numCellUnits, c.keepProbs, activation=c.activation) for c in self.rnnConfigs]
         self.add_layers(makers, self.input, (-1, -1, self.vecDim))
 
-        self.add_layers(DropoutLayer.new(self.pooledKeepProb))
+        self.add_layers(DropoutLayer.new(self.pooledKeepProb, self.pooledActivation))
         lastLayer = self.add_layers(FullyConnectedLayer.new(self.numClasses))
         self.l2Loss = self.l2RegLambda * (tf.nn.l2_loss(lastLayer.weights) + tf.nn.l2_loss(lastLayer.biases))
 
@@ -46,18 +57,9 @@ class Mark6(AbstractModel):
 
         params = [('initialLearningRate', [1e-3]),
                   ('l2RegLambda', [1e-3]),
-                  ('rnnCellUnitsNProbs', [ [([16, 13], [1, 1]), ([16, 13], [1, 1])] ]),
-                  ('pooledKeepProb', [0.5])]
-
-        cls.run_thru_data(EmbeddingDataReader, dataScale, make_params_dict(params), runScale, useCPU)
-
-    @classmethod
-    def quick_learn(cls, runScale ='small', dataScale='full_2occupations', useCPU = True):
-
-        params = [('initialLearningRate', [1e-4]),
-                  ('l2RegLambda', [1e-3]),
-                  ('rnnCellUnitsNProbs', [[([32, 16], [1, 1]), ([8], [1])]]),
-                  ('pooledKeepProb', [0.5])]
+                  ('rnnConfigs', [ [RNNConfig([16, 13], [1,1], 'relu')] ]),
+                  # ('rnnCellUnitsNProbs', [ [([16, 13], [1, 1]), ([16, 13], [1, 1])] ]),
+                  ('pooledKeepProb', [0.5]), ('pooledActivation', [None])]
 
         cls.run_thru_data(EmbeddingDataReader, dataScale, make_params_dict(params), runScale, useCPU)
 
@@ -66,13 +68,15 @@ class Mark6(AbstractModel):
         params = [('initialLearningRate', [5e-4]),
                   ('l2RegLambda', [1e-4]),
 
-                  ('rnnCellUnitsNProbs', [
-                      [([128, 8], [0.8, 1]), ([32, 8], [0.8, 1]), ([16, 8], [0.8, 1])],
-                      [([128], [0.8]), ([32], [0.8]), ([16], [0.8])],
-                      [([128, 32, 16], [0.5, 0.8, 0.8]), ([64, 64], [0.5, 0.8]), ([32], [0.8])],
+                  ('rnnConfigs', [
+                      [RNNConfig([128, 8], [0.8, 1]), RNNConfig([32, 8], [0.8, 1]), RNNConfig([16, 8], [0.8, 1])],
+                      # [([128], [0.8]), ([32], [0.8]), ([16], [0.8])],
+                      # [([128, 32, 16], [0.5, 0.8, 0.8]), ([64, 64], [0.5, 0.8]), ([32], [0.8])],
                   ]),
 
-                  ('pooledKeepProb', [1])]
+                  ('pooledKeepProb', [1]),
+                  ('pooledActivation', ['relu'])
+                  ]
 
         cls.run_thru_data(EmbeddingDataReader, dataScale, make_params_dict(params), runScale, useCPU)
 
@@ -81,15 +85,17 @@ class Mark6(AbstractModel):
         params = [('initialLearningRate', [1e-3]),
                   ('l2RegLambda', [0]),
 
-                  ('rnnCellUnitsNProbs', [
+                  ('rnnConfigs', [
                       # [([128, 8], [0.8, 1]), ([32, 8], [0.8, 1]), ([16, 8], [0.8, 1])],
                       # [([16, 128], [1, 0.8]), ([8, 32], [1, 0.8]), ([8, 16], [1, 0.8])],
                       # [([128, 64, 16], [0.5, 0.5, 1]), ([16, 64, 128], [1, 0.5, 0.5]), ([32, 32, 32], [1]*3)],
-                      [([128, 64, 16], [1]*3), ([16, 64, 128], [1]*3), ([32, 32, 32], [1]*3)],
+                      [RNNConfig([128, 64, 16], [0.5]*3), RNNConfig([16, 64, 128], [0.5]*3), RNNConfig([32, 32, 32], [0.5]*3)],
                       [([64]*4, [0.5, 0.5, 0.5, 1]), ([32]*4, [0.5, 0.5, 0.5, 1]), ([16]*4,  [0.5, 0.5, 0.5, 1])],
                   ]),
 
-                  ('pooledKeepProb', [1])]
+                  ('pooledKeepProb', [1]),
+                  ('pooledActivation', ['relu'])
+                  ]
 
         cls.run_thru_data(EmbeddingDataReader, dataScale, make_params_dict(params), runScale, useCPU)
 
